821c7677be6e820842ecc234119c2574fcc66d6a
Nouraibn <Noura_alnassar@hotmail.com>
2024-11-04 14:51:47 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Noura on 02/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
